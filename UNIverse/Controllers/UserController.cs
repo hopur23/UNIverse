201be0fe9375094d2b5094d12dc51df311e829ac
BerglindLilja <berglindliljab@gmail.com>
2015-05-08 17:26:54 +0100

    profile breytingar

--- UNIverse/Controllers/UserController.cs
+++ UNIverse/Controllers/UserController.cs
@@ -6,19 +6,30 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using UNIverse.Models;
 using UNIverse.Models.ViewModels;
+using UNIverse.Services;
 
 namespace UNIverse.Controllers
 {
     public class UserController : Controller
     {
+
+       // public ActionResult Index()
+        //{
+        //}
         public ActionResult Friends()
         {
             return View();
         }
 
-        public ActionResult Profile()
+        public ActionResult Profile(string userId)
         {
-            return View();
+            ApplicationUser user = new ApplicationUser();
+            user = ServiceWrapper.Instance.UserService.GetUserById(userId);
+
+            UserProfileViewModel viewModel = new UserProfileViewModel();
+
+            viewModel.Name = user.FirstName + " " + user.LastName;
+            return View(viewModel);
         }
 
         public ActionResult Edit()
