6cc0decf871fd66ea8100b800587ea0904e0f921
Elísa <elisarun94@gmail.com>
2015-05-14 13:16:29 +0000

    Squashed commit of the following:
    commit a8914448a54bb47119b22f71e4b8b7bbec632621
    Author: Elísa <elisarun94@gmail.com>
    Date:   Thu May 14 13:11:49 2015 +0000
    
        merge
    
    commit 4ebda0646121f9f441432d062574fad4af6f03d1
    Author: Elísa <elisarun94@gmail.com>
    Date:   Thu May 14 11:22:35 2015 +0000
    
        little fix
    
    commit 7e9b93e07fbde6d87cde6856d765ee4ffdf98f83
    Author: Elísa <elisarun94@gmail.com>
    Date:   Wed May 13 19:43:57 2015 +0000
    
        smá fix
    
    commit 70044515fd8203fe25d16d3f57dd6b2a30f84205
    Author: Elísa <elisarun94@gmail.com>
    Date:   Wed May 13 19:28:34 2015 +0000
    
        Sort by tags
    
    commit 07160c448c9d8cc4c19c690a1068b20113a98d1c
    Author: Elísa <elisarun94@gmail.com>
    Date:   Wed May 13 16:20:20 2015 +0000
    
        Newsfeed scroll virkni

---
+++ UNIverse/Controllers/BasePagingController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UNIverse.Services;
+using UNIverse.Models;
+using UNIverse.Models.ViewModels;
+using Microsoft.AspNet.Identity;
+
+namespace UNIverse.Controllers
+{
+    public class BasePagingController : Controller
+    {
+        public ActionResult PostsPartial()
+        {
+            var model = GetModel(0);
+            return PartialView("PostsPartial", model);
+        }
+
+        public JsonResult CheckPostsStatus(int currentPageNumber)
+        {
+            var posts = ServiceWrapper.PostService.GetAllPostsFromFriends(this.User.Identity.GetUserId());
+            var totalPages = (int)Math.Ceiling((decimal)posts.Count() / 15);
+            return Json((currentPageNumber + 1) < totalPages, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult LoadMorePosts(int currentPageNumber)
+        {
+            var model = GetModel(currentPageNumber);
+            return PartialView("PostDisplayer", model);
+        }
+
+        protected PostViewModel GetModel(int currentPageNumber)
+        {
+            var posts = ServiceWrapper.PostService.GetAllPostsFromFriends(this.User.Identity.GetUserId());
+            if (currentPageNumber == 0)
+            {
+                return new PostViewModel
+                {
+                    Posts = posts.Take(NumberOfPosts).ToList()
+                };
+            }
+
+            var currentPage = currentPageNumber + 1;
+            return new PostViewModel
+            {
+                Posts = posts.Skip((currentPage - 1) * NumberOfPosts).Take(NumberOfPosts).ToList()
+            };
+        }
+
+        private const int NumberOfPosts = 15;
+    }
+}
--- UNIverse/Controllers/HomeController.cs
+++ UNIverse/Controllers/HomeController.cs
@@ -13,13 +13,41 @@ namespace UNIverse.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-       
-        public ActionResult Index()
-        {
-            // TODO: Sækja frekar latest posts frekar en alla. Implementa filters
-            var model = ServiceWrapper.PostService.GetAllPostsFromFriends(this.User.Identity.GetUserId());
+        private const int defaultEntryCount = 10;
 
-            return View(model);
+        public ActionResult Index(int? postToID)
+        {
+            if (!postToID.HasValue)
+                postToID = defaultEntryCount;
+
+            if(Request.IsAjaxRequest())
+            {                
+                //Retrieve the page specified by the page variable with a page size o defaultEntryCount
+                PostViewModel pagedEntries = GetModel(postToID.Value);        
+        
+                return PartialView("PostPage", pagedEntries);
+            }
+
+            //Retrieve the first page with a page size of entryCount
+            PostViewModel entries = GetModel(0);
+                
+            AddMoreUrlToViewData();
+
+            return View("Index", entries);
+        }
+
+     /*   {
+            // TODO: Sækja frekar latest posts frekar en alla. Implementa filters
+        //    var modeld = ServiceWrapper.PostService.GetAllPostsFromFriends(this.User.Identity.GetUserId());
+            var model = GetModel(0);
+            return View("Index", model);
+        
+           // return View(model);
+        }*/
+
+        private void AddMoreUrlToViewData()
+        {
+            ViewData["moreUrl"] = Url.Action("Index", "Home");
         }
 
         public ActionResult Search(string searchString)
@@ -56,5 +84,54 @@ namespace UNIverse.Controllers
             return View(model);
         }
 
+
+        protected PostViewModel GetModel(int toID = 0)
+        {
+            var posts = ServiceWrapper.PostService.GetAllPostsFromFriends(this.User.Identity.GetUserId());
+            if (toID == 0)
+            {
+                return new PostViewModel
+                {
+                    Posts = posts.Take(defaultEntryCount).ToList()
+                };
+            }
+            
+            return new PostViewModel
+            {
+                Posts = posts.Where(p => p.Id < toID).Take(defaultEntryCount).OrderByDescending(m => m.Id).ToList()
+            };
+        }
+
+        public ActionResult Tag(string tag)
+        {
+            PostViewModel viewModel = new PostViewModel();
+            if (!String.IsNullOrEmpty(tag))
+            {
+                var posts = ServiceWrapper.PostService.PostsByTag(tag);
+                viewModel = GetView(posts);
+            }
+
+            return View("Index", viewModel);
+        }
+
+       /* public ActionResult Groups()
+        {
+            var posts = ServiceWrapper.PostService.PostsByGroups(this.User.Identity.GetUserId());
+            PostViewModel viewModel = GetView(posts);
+
+            return View("Index", viewModel);
+        }*/
+
+        public PostViewModel GetView(List<Post> list)
+        {
+            if (list != null)
+            {
+                return new PostViewModel
+                {
+                    Posts = list.ToList()
+                };
+            }
+            return new PostViewModel();
+        }
     }
 }
